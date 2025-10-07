3588ee2feb0303e4d44d7229c6d2c9cb6cb156a3
Fowlerpro <jaycefowler@outlook.com>
2025-10-06 21:34:30 -0400

    add Basic start menu Function

--- MohawkTerminalGame/TerminalGame.cs
+++ MohawkTerminalGame/TerminalGame.cs
@@ -1,4 +1,7 @@
-﻿namespace MohawkTerminalGame;
+﻿using System;
+using System.Data;
+
+namespace MohawkTerminalGame;
 
 public class TerminalGame
 {
@@ -6,12 +9,18 @@ public class TerminalGame
 
 
     /// Run once before Execute begins
+    //Varaibles
+    public bool startGame = false;
     public void Setup()
     {
-        Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteOnce;
-        Program.TerminalInputMode = TerminalInputMode.KeyboardReadAndReadLine;
+
+        Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteTime;
+        Program.TerminalInputMode = TerminalInputMode.EnableInputDisableReadLine;
+        Program.TargetFPS = 60;
 
         Terminal.SetTitle("Tales from the Past");
+        intro();
+
     }
 
     // Execute() runs based on Program.TerminalExecuteMode (assign to it in Setup).
@@ -21,7 +30,51 @@ public class TerminalGame
     //               Code must finish within the alloted time frame for this to work well.
     public void Execute()
     {
-
+        if (!startGame)
+        {
+            if (Input.IsKeyPressed(ConsoleKey.Spacebar))
+            {
+                startGame = true;
+                startArea();
+                Console.WriteLine(startGame);
+            }
+        }
+        if (Input.IsKeyPressed(ConsoleKey.RightArrow))
+            Console.WriteLine(startGame);
     }
+    public void Update()
+    {
+        if (Input.IsKeyPressed(ConsoleKey.RightArrow))
+            Console.WriteLine(startGame);
+    }
+    public void intro()
+    {
+        ColoredText empty = new(@",", ConsoleColor.Green, ConsoleColor.DarkGreen);
+        ColoredText borderL = new(@"||", ConsoleColor.White, ConsoleColor.DarkGreen);
+        ColoredText borderR = new(@"||", ConsoleColor.White, ConsoleColor.DarkGreen);
+        ColoredText borderB = new(@"==", ConsoleColor.White, ConsoleColor.DarkGreen);
+        ColoredText borderT = new(@"==", ConsoleColor.White, ConsoleColor.DarkGreen);
+        TerminalGridWithColor map;
+        map = new(50, 50, empty);
+        map.SetCol(borderL, 0);
+        map.SetCol(borderR, 49);
+        map.SetRow(borderB, 1);
+        map.SetRow(borderT, 49);
+        map.ClearWrite();
+    }
+    public void startArea()
+    {
+        // Set map to some values
+        ColoredText tree = new(@"/\", ConsoleColor.Green, ConsoleColor.DarkGreen);
+        ColoredText riverNS = new(@"||", ConsoleColor.Blue, ConsoleColor.DarkBlue);
+        ColoredText riverEW = new(@"==", ConsoleColor.Blue, ConsoleColor.DarkBlue);
+        ColoredText player = new(@"😎", ConsoleColor.White, ConsoleColor.Black);
+        TerminalGridWithColor map;
+        map = new(10, 10, tree);
+        map.SetCol(riverNS, 3);
+        map.SetRow(riverEW, 8);
 
+        // Clear window and draw map
+        map.ClearWrite();
+    }
 }
