2c4ba7c3dd998f95931e43a087b825f74c79618b
Shaft0509 <sergionieto555@gmail.com>
2025-10-06 23:53:02 -0400

    Scratched off the classes idea as it is too much of a scope and just narrowed it down to the single TerminalGame.cs, added character inicial positions and symbols.

--- MohawkTerminalGame/Knight.cs
+++ MohawkTerminalGame/Knight.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using MohawkTerminalGame;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DefaultNamespace
 {
-    internal class Knight
+    internal class Knight 
     {
+        
     }
 }
--- MohawkTerminalGame/TerminalGame.cs
+++ MohawkTerminalGame/TerminalGame.cs
@@ -1,27 +1,24 @@
-﻿namespace MohawkTerminalGame;
+﻿using DefaultNamespace;
+using System;
 
-public class TerminalGame
+namespace MohawkTerminalGame
 {
-    // Place your variables here
-
-
-    /// Run once before Execute begins
-    public void Setup()
+    public class TerminalGame
     {
-        Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteOnce;
-        Program.TerminalInputMode = TerminalInputMode.KeyboardReadAndReadLine;
+        TerminalGridWithColor map;
 
-        Terminal.SetTitle("Tales from the Past");
-    }
+        // ---------------- Characters ----------------
+        ColoredText knightChar = new(@"⚔", ConsoleColor.White, ConsoleColor.Black);
+        int knightX = 0, knightY = 0;
+        int oldKnightX = 0, oldKnightY = 0;
 
-    // Execute() runs based on Program.TerminalExecuteMode (assign to it in Setup).
-    //  ExecuteOnce: runs only once. Once Execute() is done, program closes.
-    //  ExecuteLoop: runs in infinite loop. Next iteration starts at the top of Execute().
-    //  ExecuteTime: runs at timed intervals (eg. "FPS"). Code tries to run at Program.TargetFPS.
-    //               Code must finish within the alloted time frame for this to work well.
-    public void Execute()
-    {
+        ColoredText witchChar = new(@"⚔", ConsoleColor.Magenta, ConsoleColor.Black);
+        int witchX = 2, witchY = 0;
+        int oldWitchX = 2, oldWitchY = 0;
+
+        ColoredText thiefChar = new(@"⚔", ConsoleColor.DarkGray, ConsoleColor.Black);
+        int thiefX = 4, thiefY = 0;
+        int oldThiefX = 4, oldThiefY = 0;
 
     }
-
 }
